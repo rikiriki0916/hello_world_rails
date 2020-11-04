a235bfcc02e953dd2e940ef524f7691256028702
白藤　健輔 <>
2020-11-04 23:27:49 +0900

    crud処理のread編を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
+    binding.pry
     @users = User.all
-
     render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
