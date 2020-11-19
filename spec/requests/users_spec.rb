78e3f6374d6c282cc790af978c23ab31417361d3
白藤　健輔 <>
2020-11-20 00:36:22 +0900

    Request Spec を書こう【index 編】

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "ユーザーの一覧が取得できる" do
+    subject { get(users_path) }
+    before { 3.times { create(:user) } }
+
+    fit "ユーザーの一覧が取得できる" do
+
+      # get users_path
+      subject
+
+      binding.pry
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["account", "name", "email"]
+      expect(response).to have_http_status(200)
     end
   end
 
